0c6e0dcbd38d201353129081e28856b51fe16e9e
liping <Macx@MacxdeMacBook-Pro.local>
2017-12-20 15:45:42 +0800

    提交信息

---
+++ First/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  First
+//
+//  Created by Macx on 2017/10/26.
+//  Copyright © 2017年 Macx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let index = 10;
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
